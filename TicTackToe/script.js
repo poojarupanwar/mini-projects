bd3fdc8175b6a48a71e471bf700b6cc2711e4cc7
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-05-20 16:56:44 +0530

    tic tack toe index file added

---
+++ TicTackToe/script.js
@@ -0,0 +1,19 @@
+console.log("Welcome To tic tack toe");
+let music=new Audio("music.mp3")
+let turnMusic=new Audio("ting.mp3");
+let gameover=new Audio("gameover.mp3");
+let turn="X";
+
+//function to cham
+const changeTurn=()=>{
+    return turn==='X'?"0":"X"
+}
+
+//function to check for win
+const checkWin=()=>{
+
+}
+
+///Main logic start
+
+ 
