a5301c32fb5ed8d757d3c7f8550d20278b185ab9
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-08-27 20:33:31 +0530

    Update Script.jsx

--- TicTackToe/script.js
+++ TicTackToe/script.js
@@ -1,12 +1,12 @@
 console.log("Welcome To tic tack toe");
 let music=new Audio("music.mp3")
-let turnMusic=new Audio("ting.mp3");
+let audioTurn=new Audio("ting.mp3");
 let gameover=new Audio("gameover.mp3");
 let turn="X";
 
 //function to cham
 const changeTurn=()=>{
-    return turn==='X'?"0":"X"
+    return turn==="X"?"0":"X"
 }
 
 //function to check for win
@@ -17,15 +17,15 @@ const checkWin=()=>{
 ///Game logic start
 let boxes=document.getElementsByClassName("box")
 
-Array.from(boxes).forEach(e=>{
+Array.from(boxes).forEach(element=>{
     let boxtext=element.querySelector('.boxtext')
     element.addEventListener('click',()=>{
         if(boxtext.innerText==" "){
-            boxtext.innerText="X";
-            changeTurn();
+            boxtext.innerText=turn;
+            turn=changeTurn();
             audioTurn.play()
             checkWin()
-            document.getElementsByClassName("turn")[0].innerText="turn for"+ turn;
+            document.getElementsByClassName("info")[0].innerText="turn for"+ turn;
         }
     })
 
